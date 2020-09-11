ce1877c4745a42dcaf35858edc23f370f47e8c3e
cdde <3018231-cdde@users.noreply.gitlab.com>
2020-09-11 17:42:14 -0500

    Commit changes

--- modules/BasicTheme/Main.php
+++ modules/BasicTheme/Main.php
@@ -1,24 +1,15 @@
 <?php
 
 function rightBarContent() {
-    echo "
-</div>
-<div class=\"right-bar-div\">";
-    echo "right bar content";
-    echo "</div></div>";
+    global $IP;
+    
+    require($IP . "/data/xml/bar.xml");
 }
 
 function ThemeStart() {
-    echo "
-<html>
-    <head>
-        <title>Vwikis</title>
-        <link rel=\"stylesheet\" href=\"./data/css/style.css\"/>
-    </head>
-    <body>
-        <div class=\"full-div\">
-            <div class=\"left-bar-div\">
-    ";
+    global $IP;
+    
+    require($IP . "/data/xml/header.xml");
 }
 
 function ThemeEnd() {
